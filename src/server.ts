3901e3406af35469a744799fd015dc96e420d126
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2023-06-01 17:48:11 -0400

    :building_construction: Convert js files to ts

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
-import * as express from 'express';
+import express from "express";
 // import * as cors from 'cors';
-import * as bodyParser from 'body-parser';
-import { getOneAthlete, createAthlete } from './models/athletes';
-import { createNotification, updateNotification } from './models/notifications';
+import bodyParser from "body-parser";
+import { getOneAthlete, createAthlete } from "./models/athletes";
+import { createNotification, updateNotification } from "./models/notifications";
 
 let PORT = 5500;
 
@@ -10,25 +10,31 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
+app.use((_, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", 'POST, GET, PUT, DELETE, OPTIONS');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
 // TODO: build a jwt verification middleware (or find a package) -- this will be in one level higher -- gateway
 
-app.get("/api/v0/athlete", async (req, res) => {
-  const { first, last, suffix } = req.query;
-  try {
-    const athlete = await getOneAthlete(first, last, suffix);
-    res.json(athlete)
-  } catch (e) {
-    console.log('Error getting one athlete: ', e);
-    res.sendStatus(404);
+app.get<{}, "", "", { first: string; last: string; suffix: string }>(
+  "/api/v0/athlete",
+  async (req, res) => {
+    const { first, last, suffix } = req.query;
+    try {
+      const athlete = await getOneAthlete(first, last, suffix);
+      res.json(athlete);
+    } catch (e) {
+      console.log("Error getting one athlete: ", e);
+      res.sendStatus(404);
+    }
   }
-});
+);
 
 app.post("/api/v0/athletes(/:athleteId)?", async (req, res) => {
   const { firstName, lastName, suffix } = req.body;
@@ -37,26 +43,25 @@ app.post("/api/v0/athletes(/:athleteId)?", async (req, res) => {
     // should post send back athlete?
     res.sendStatus(200);
   } catch (e) {
-    console.log('Error in createAthlete: ', e);
+    console.log("Error in createAthlete: ", e);
     res.sendStatus(400);
   }
 });
 
 // post - send notification
 // update - accept/reject notification
-  // if the notification is a friend request, insert into friends table
-  // if the notification is a competition invite, figure out that logic
-  // what other notifications might we have? will all we "interactive"?
-
+// if the notification is a friend request, insert into friends table
+// if the notification is a competition invite, figure out that logic
+// what other notifications might we have? will all we "interactive"?
 
 // body: { notificationName: ["friend", "competition", "etc."], sender: "knissley", recipient: "esabini" }
 app.post("/api/v0/notification", async (req, res) => {
-  const {notificationName, senderName, recipientName} = req.body;
+  const { notificationName, senderName, recipientName } = req.body;
   try {
     await createNotification(notificationName, senderName, recipientName);
     res.sendStatus(200);
   } catch (e) {
-    console.log('Error in notification post: ', e);
+    console.log("Error in notification post: ", e);
     res.sendStatus(400);
   }
 });
@@ -72,7 +77,7 @@ app.patch("/api/v0/notification/:notificationId", async (req, res) => {
     await updateNotification(notificationId, newStatus);
     res.sendStatus(200);
   } catch (e) {
-    console.log('Error updating notification: ', e);
+    console.log("Error updating notification: ", e);
     res.sendStatus(400);
   }
 });
@@ -91,4 +96,4 @@ app.patch("/api/v0/notification/:notificationId", async (req, res) => {
 // });
 
 console.log(`Listening on ${PORT}...`);
-app.listen(PORT)
+app.listen(PORT);
