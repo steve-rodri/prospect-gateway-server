2687aecc8935c424f1b007c5037f75ef91861610
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2023-06-04 09:07:55 -0400

    :sparkles: Create protectedProcedure, setup auth middleware

--- src/trpc/context.ts
+++ src/trpc/context.ts
@@ -1,9 +1,24 @@
+import { PrismaClient } from "@prisma/client"
 import { inferAsyncReturnType } from "@trpc/server"
-import * as trpcExpress from "@trpc/server/adapters/express"
+import { CreateExpressContextOptions } from "@trpc/server/adapters/express"
 
-export const createContext = ({
-	req,
-	res
-}: trpcExpress.CreateExpressContextOptions) => ({}) // no context
+type ContextOpts = CreateExpressContextOptions
+
+export const createContext = async ({ req }: ContextOpts) => {
+	const prisma = new PrismaClient()
+
+	const getUserFromHeader = async () => {
+		if (req.headers.authorization) {
+			// const user = await decodeAndVerifyJwtToken(
+			// 	req.headers.authorization.split(" ")[1]
+			// )
+			// return user
+		}
+		return null
+	}
+
+	const user = await getUserFromHeader()
+	return { user, prisma }
+} // no context
 
 export type Context = inferAsyncReturnType<typeof createContext>
