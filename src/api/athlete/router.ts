bb22556e8748f11a40d85b28f0093f5b99c12f3c
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2023-06-04 08:30:06 -0400

    :building_construction: Setup trpc

--- src/api/athlete/router.ts
+++ src/api/athlete/router.ts
@@ -1,23 +1,29 @@
 import express, { Router } from "express"
+import { publicProcedure, router } from "../../trpc"
 
 import * as AthleteRequestHandlers from "./request-handlers"
 // import { requiredScopes } from 'express-oauth2-jwt-bearer'
 // import { permissionsType } from '../../constants'
 
-// /api/v0/athlete
-export const createAthleteRouter = (): Router => {
-	const router = express.Router()
+export const athleteRouter = router({
+	getAthletes: publicProcedure.query(() => "get Athletes"),
+	createAthlete: publicProcedure.mutation(() => "create Athlete"),
+	findOrCreateAthlete: publicProcedure.query(() => "find or create Athlete"),
+	findOneAthlete: publicProcedure.query(() => "find one Athlete")
+})
 
-	// Scopes
-	// const readScope = requiredScopes(permissionsType.moviesRead)
-	// const writeScope = requiredScopes(permissionsType.moviesWrite)
-	// const deleteScope = requiredScopes(permissionsType.moviesDelete)
+// // /api/v0/athlete
+// export const createAthleteRouter = (): Router => {
+// 	// const router = express.Router()
+// 	// Scopes
+// 	// const readScope = requiredScopes(permissionsType.moviesRead)
+// 	// const writeScope = requiredScopes(permissionsType.moviesWrite)
+// 	// const deleteScope = requiredScopes(permissionsType.moviesDelete)
+// 	// router.get("/", AthleteRequestHandlers.find())
+// 	// router.post("/", AthleteRequestHandlers.create())
+// 	// router.get("/search", AthleteRequestHandlers.findOrCreate())
+// 	// router.get("/:id", AthleteRequestHandlers.findOne())
+// 	// return router
+// }
 
-	router.get("/", AthleteRequestHandlers.find())
-	router.post("/", AthleteRequestHandlers.create())
-	router.get("/search", AthleteRequestHandlers.findOrCreate())
-	router.get("/:id", AthleteRequestHandlers.findOne())
-	return router
-}
-
-export default createAthleteRouter()
+// export default createAthleteRouter()
--- src/createServer.ts
+++ src/createServer.ts
@@ -1,65 +1,29 @@
-import express from "express"
+import { PrismaClient } from "@prisma/client"
+import { createExpressMiddleware } from "@trpc/server/adapters/express"
 import compression from "compression"
 import cors from "cors"
-// import swaggerUi from "swagger-ui-express";
-
-import { ApplicationServer } from "./types"
-import { loggerMiddleware } from "./logger"
+import express from "express"
 import http from "http"
-import athleteRouter from "./api/athlete/router"
-import notificationRouter from "./api/notification/router"
-import { PrismaClient } from "@prisma/client"
-// import { swaggerDocument, swaggerOptions } from "./swaggerOptions";
-// import authenticationMiddleware from "./middleware/authentication";
 
-async function createServer(): Promise<ApplicationServer> {
+import { loggerMiddleware } from "./logger"
+import { appRouter, createContext } from "./trpc"
+import { ApplicationServer } from "./types"
+
+const trpcExpressMiddleware = createExpressMiddleware({
+	router: appRouter,
+	createContext
+})
+
+export const createServer = async (): Promise<ApplicationServer> => {
 	const app = express()
-
-	// Middlewares
-	app.use(express.json()) // To parse the incoming requests with JSON payloads
-
-	// TODO: build a jwt verification middleware (or find a package) -- this will be in one level higher -- gateway
-	// // Authentication
-	// app.use('/api/*', authenticationMiddleware)
-
-	// Logger
-	if (process.env.NODE_ENV !== "TEST") {
-		app.use(loggerMiddleware)
-	}
-
-	// CORS allowed origin and headers
+	if (process.env.NODE_ENV !== "TEST") app.use(loggerMiddleware)
+	app.use(express.json())
 	app.use(cors())
-	// app.use((_, res, next) => {
-	//   res.header("Access-Control-Allow-Origin", "*");
-	//   res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
-	//   res.header(
-	//     "Access-Control-Allow-Headers",
-	//     "Origin, X-Requested-With, Content-Type, Accept"
-	//   );
-	//   next();
-	// });
-
-	// Compression
 	app.use(compression())
-
-	// Routing
-	app.use("/api/v0/athletes", athleteRouter)
-	app.use("/api/v0/notifications", notificationRouter)
-
-	const httpServer = http.createServer(app)
-
-	// // Swagger docs
-
-	// app.get('/api-docs/swagger.json', (_, res) => res.json(swaggerDocument))
-	// app.use(
-	//   '/api-docs',
-	//   swaggerUi.serveFiles(undefined, swaggerOptions),
-	//   swaggerUi.setup(undefined, swaggerOptions)
-	// )
-
+	app.use("/trpc", trpcExpressMiddleware)
 	return {
-		app: httpServer,
-		dbClient: new PrismaClient()
+		app: http.createServer(app),
+		prisma: new PrismaClient()
 	}
 }
 
